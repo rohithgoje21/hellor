e451a63c59d8820197ff215e0992eb29b1984ea6
rohithgoje21 <rohithgoje21@gmail.com>
2024-11-02 20:26:04 +0530

    Initial commit

--- server.js
+++ server.js
@@ -49,6 +49,7 @@ app.post("/results", async (req, res) => {
   }
 });
 // Start the server
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Server running at http://localhost:${port}`);
+// });
+module.exports = app;
